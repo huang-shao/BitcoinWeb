79bff757a9ac30d9ab5588b41b7e39c0a4d6d749
wenb1125 <2424624564@qq.com>
2020-12-31 09:44:32 +0800

    second commit

--- main.go
+++ main.go
@@ -3,13 +3,11 @@ package main
 import (
 	"beegoText/db_mysql"
 	_ "beegoText/routers"
-	"github.com/astaxie/beego"
 )
 
 func main() {
-	//1.连接数据库
+	//1.连接数据库,
 	db_mysql.ConnectDB()
 
-	beego.Run()
-}
+ }
 
