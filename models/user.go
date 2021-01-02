5f8d98378b58a6cd7c8ff7b7d8bbf0c4e5ce7184
hxldsl99 <h2280741965@qq.com>
2021-01-02 23:43:53 +0800

    ljx`s merge

---
+++ models/user.go
@@ -0,0 +1,52 @@
+package models
+
+import (
+	"BitcoinWeb/db_mysql"
+	"BitcoinWeb/utils"
+)
+
+type User struct {
+	Id       int    `form:"id"`
+	Phone    string `form:"phone"`
+	Password string `form:"password"`
+}
+
+/**
+ * 保存用户信息的方法：保存用户信息到数据库中
+ */
+func (u User) SaveUser() (int64, error) {
+	//1、密码脱敏处理
+	u.Password = utils.MD5HashString(u.Password)
+
+	//2、执行数据库操作
+
+
+
+
+	row, err := db_mysql.Db.Exec("insert into user(phone, password) "+
+		" values(?,?) ", u.Phone, u.Password)
+	if err != nil {
+		return -1, err
+	}
+	id, err := row.RowsAffected()
+	if err != nil {
+		return -1, err
+	}
+	return id, nil
+}
+
+/**
+ * 查询用户信息
+ */
+func (u User) QueryUser() (*User, error) {
+	//1、密码脱敏处理
+	u.Password = utils.MD5HashString(u.Password)
+
+	row := db_mysql.Db.QueryRow("select phone, password from user where  phone = ? and password = ? ",
+		u. Phone,u.Password)
+	err := row.Scan(&u.Phone, &u.Password)
+	if err != nil {
+		return nil, err
+	}
+	return &u, nil
+}
