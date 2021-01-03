3b03c9a6018ab5067e92caeb29ab1dcbb439927b
hxldsl99 <h2280741965@qq.com>
2021-01-03 15:28:03 +0800

    Last push

--- controllers/userShow.go
+++ controllers/userShow.go
@@ -12,11 +12,7 @@ type ShowTime struct {
 	beego.Controller
 }
 
-
-
-
-
-func (s *ShowTime) Post()  {
+func (s *ShowTime) Post() {
 	var comd entity.RpcCommand
 	err := s.ParseForm(&comd)
 	if err != nil {
@@ -25,48 +21,22 @@ func (s *ShowTime) Post()  {
 	}
 	fmt.Println("命令为：", comd.Command)
 	fmt.Println("参数一为：", comd.First_parameter)
-	fmt.Println("参数二为：",comd.Second_parameter)
-
-	//if comd.First_parameter==0 {
-	//	data1:=utils.PrepareJSON(comd.Command,comd.Second_parameter)
-	//	t.Data["Data"]=data1
-	//}else {
-	//
-	//	data:=utils.PrepareJSON(comd.Command,comd.First_parameter)
-	//
-	//	t.Data["Data"]=data
-	//}
+	fmt.Println("参数二为：", comd.Second_parameter)
 
 	var data interface{}
 	switch comd.Command {
-	case "getblockhash" ,"getblockfilter":
+	case "getblockhash", "getblockfilter":
 		height, err := strconv.Atoi(comd.First_parameter)
 		if err != nil {
 			s.Ctx.WriteString("参数类型有误，请重新输入！")
 			return
 		}
 		data = utils.PrepareJSON(comd.Command, height)
-	case "getblock", "gettransaction","addnode":
+	case "getblock", "gettransaction", "addnode":
 		data = utils.PrepareJSON(comd.Command, comd.First_parameter)
-	case "getbestblockhash","getblockchaininfo","getblockcount","getchaintips","getdifficulty","getmempoolinfo","gettxoutsetinfo","savemempool","verifychain","getrpcinfo","help","stop","uptime","getmininginfo","getnewadress":
+	case "getbestblockhash", "getblockchaininfo", "getblockcount", "getchaintips", "getdifficulty", "getmempoolinfo", "gettxoutsetinfo", "savemempool", "verifychain", "getrpcinfo", "help", "stop", "uptime", "getmininginfo", "getnewadress":
 		data = utils.PrepareJSON(comd.Command)
 	}
-
-
-
-
-	//switch comd.Command {
-	//case "":
-	//	height, err := strconv.Atoi(comd.Second_parameter)
-	//	if err != nil {
-	//		s.Ctx.WriteString("参数类型有误，请重新输入！")
-	//		return
-	//	}
-	//	data = utils.PrepareJSON(comd.Command, comd.First_parameter, height)
-	//case "addnode":
-	//	data = utils.PrepareJSON(comd.Command, comd.First_parameter, comd.Second_parameter)
-	//
-	//}
 
 	s.Data["Data"] = data
 
